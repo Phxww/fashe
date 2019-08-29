ab6177a2c8586b8760479fbef3acb3714c6d05f7
Phwxx <wwbphx@gmail.com>
2019-08-29 14:23:51 +0800

    modify db setting

--- connection/db_options.js
+++ connection/db_options.js
@@ -3,10 +3,10 @@
 const options = {
     client: 'pg',
     connection: {
-      host: process.env.HOST ,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database:  process.env.DB
+      host: process.env.DATABASE_HOST ,
+      user: process.env.DATABASE_LOGIN_USER ,
+      password: process.env.DATABASE_LOGIN_PASS,
+      database:  process.env.DATABASE_NAME
     }
   };
 // const knex = require('knex')(options);
